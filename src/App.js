fa383312fd1e975717b2eb3586edbce574b82726
77970 <avengershahnwaz1@gmail.com>
2022-03-18 13:31:33 +0530

    Home page

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import SignUp from './pages/accounts/SignUp';
 import SignIn from './pages/accounts/SignIn';
 import ForgetPassword from './pages/accounts/ForgetPassword';
 import Headera from './pages/home/Header';
+import Home from './pages/home/Home';
 const App=()=>{
   return (
     <>
 <Headera/>
 {/* <ForgetPassword/> */}
+<Home/>
+{/* <SignUp /> */}
 </>
   );
 }
--- src/pages/home/Header.js
+++ src/pages/home/Header.js
@@ -3,8 +3,10 @@ import './Header.css';
 const Header =() =>{
     return (
         <div className='Header__main'>
+            <div className='Header__desc'>
             <img src='../../../College LOGO.png'/>
             <h2>Acharya Narendra Dev College</h2>
+            </div>
             <div className='Header__nav'>
                 <ul>
                     <li><a><b>Home</b></a></li>
